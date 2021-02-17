fb6f3a3df16efa32778b74d2205cad60cb8bf765
onanalee <69330162+onanalee@users.noreply.github.com>
2021-02-17 15:50:37 +0900

    Products HTML pages added

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template('index.html')
 
 @app.route('/about_us')
-def about():
+def about_us():
     return render_template('about_us.html')
 
 @app.route('/applications')
@@ -24,12 +24,40 @@ def products():
     return render_template('products.html')
 
 @app.route('/tech_support')
-def timezone():
+def tech_support():
     return render_template('tech_support.html')
 
 @app.route('/contact_us')
-def exchangeRates():
+def contact_us():
     return render_template('contact_us.html')
+
+@app.route('/products/fans_blowers')
+def products_fans_blowers():
+    return render_template('products_fans_blowers.html')
+
+@app.route('/products/heatsink')
+def products_heatsink():
+    return render_template('products_heatsink.html')
+
+@app.route('/products/industrial_automation')
+def products_industrial_automation():
+    return render_template('products_industrial_automation.html')
+
+@app.route('/products/motor_components')
+def products_motor_components():
+    return render_template('products_motor_components.html')
+
+@app.route('/products/motors')
+def products_motors():
+    return render_template('products_motors.html')
+
+@app.route('/products/power_supplies')
+def products_power_supplies():
+    return render_template('products_power_supplies.html')
+
+@app.route('/products/wire_harness')
+def products_wire_harness():
+    return render_template('products_wire_harness.html')
 
 
 if __name__ == '__main__':
