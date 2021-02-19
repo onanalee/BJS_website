90384a8c5fb78beb7dcd52c2a88209f5e2931386
onanalee <69330162+onanalee@users.noreply.github.com>
2021-02-19 15:54:15 +0900

    Applications, About Us implemented

--- app.py
+++ app.py
@@ -60,5 +60,10 @@ def products_wire_harness():
     return render_template('products_wire_harness.html')
 
 
+@app.route('/dc_fans')
+def dc_fans():
+    return render_template('dc_fans.html')
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
